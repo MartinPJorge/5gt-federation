a84a04b927139caac723d16833f1454f055c795b
Jorge Martín Pérez <jmartinp@it.uc3m.es>
2020-06-11 16:26:10 +0200

    Move AWS env to separate file
     * prevent division by 0 in get_state

--- utils/aws/dqn.py
+++ utils/aws/dqn.py
@@ -8,359 +8,12 @@ import tensorflow as tf
 from tensorflow import keras
 import random
 import math
+from aws_env import AWS_env
 
 
 
 # print("TensorFlow version: {}".format(tf.__version__))
 # print("Eager execution: {}".format(tf.executing_eagerly()))
-
-
-
-# ACTIONs macros
-A_LOCAL = 0
-A_FEDERATE = 1
-A_REJECT = 2
-ACTIONS = [A_LOCAL, A_FEDERATE, A_REJECT]
-
-# FUTURE
-FUTURE = pd.Timestamp('2200-01-01 00:00:00+00:00')
-
-
-
-class AWS_env():
-    def __init__(self, cpu, memory, disk, f_cpu, f_memory, f_disk,
-                 arrivals, spot_prices):
-        # arrivals: pandas DataFrame with columns
-        #           ['time', 'instance', 'spotprice', 'cpu', 'memory',
-        #            'os', 'disk', 'reward', 'lifetime']
-        #           the reward is expressed as $/hour
-        #           lifetime in days
-        #           time is the UNIX epoch in seconds since 1970
-        # spot_prices: pandas DataFrame with columns
-        #              ['AvailabilityZone', 'InstanceType',
-        #               'ProductDescription', 'SpotPrice', 'Timestamp']
-
-        self.cpu, self.max_cpu = cpu, cpu
-        self.disk, self.max_disk = disk, disk
-        self.memory, self.max_memory = memory, memory
-        self.f_cpu, self.max_f_cpu = f_cpu, f_cpu
-        self.f_disk, self.max_f_disk = f_disk, f_disk
-        self.f_memory, self.max_f_memory = f_memory, f_memory
-        self.time = float(0)
-        self.capacity = [int(cpu), int(memory), int(disk)]
-        self.f_capacity = [int(f_cpu), int(f_memory), int(f_disk)] if\
-            f_cpu != math.inf else [math.inf, math.inf, math.inf]
-        self.profit = float(0)
-        self.arrivals = arrivals
-        self.service_length = []
-        self.total_num_services = int(0)
-        # INDEXING
-        self.curr_idx = 0 # current index in the arrivals DataFrame
-        self.in_local = [] # list of arrival indexes in local domain
-        self.in_fed = [] # list of arrival indexes in federated domain
-        # ARRIVAL reward
-        self.max_reward = 0
-        # SPOT PRICE
-        self.spot_prices = spot_prices
-        self.spot_prices['Timestamp'] =\
-            pd.to_datetime(self.spot_prices['Timestamp'])
-        self.max_spot_prices = {
-            i: 0
-            for i in spot_prices['InstanceType'].unique()
-        }
-
-
-
-    def get_state(self):
-        if self.curr_idx == len(self.arrivals) - 1:
-            return None
-
-        arrival = self.arrivals.iloc[self.curr_idx]
-        instance = arrival['instance']
-        arrival_t = pd.Timestamp(arrival['time'], unit='s', tz='UTC')
-
-        self.max_reward = max(self.max_reward, arrival['reward'])
-
-        # Find what is the latest spot price for that instance
-        spot_price_df = self.spot_prices[
-                    self.spot_prices['InstanceType'] == instance]
-        spot_price_df.sort_values(by='Timestamp', ascending=True, inplace=True)
-        past_prices = spot_price_df[spot_price_df['Timestamp'] <= arrival_t]
-        if len(past_prices) == 0:
-            spot_price = spot_price_df.iloc[0]['SpotPrice']
-        else:
-            spot_price = past_prices.iloc[-1]['SpotPrice']
-        
-        # Update the max spot price for that instance
-        if spot_price > self.max_spot_prices[instance]:
-            self.max_spot_prices[instance] = spot_price
-
-
-        return [
-            # Local resources
-            self.cpu    / self.max_cpu,
-            self.disk   / self.max_disk,
-            self.memory / self.max_memory,
-            # Federated resources
-            self.f_cpu    / self.max_f_cpu,
-            self.f_disk   / self.max_f_disk,
-            self.f_memory / self.max_f_memory,
-            # Arrival local resources consumption
-            arrival['cpu']    / self.cpu,
-            arrival['disk']   / self.disk,
-            arrival['memory'] / self.memory,
-            # Arrival reward per hour
-            arrival['reward'] / self.max_reward,
-            # Arrival spot price
-            spot_price / max(p for p in self.max_spot_prices.values())
-        ]
-
-        # return [self.cpu, self.disk, self.memory,
-        #         self.f_cpu, self.f_disk, self.f_memory]
-
-
-    def take_action(self, action):
-        # a={1,2,3} local deployment, federate, reject
-        # returns the reward and next state
-        # in case it is the last state, it returns None as state
-
-        # t = t + 1
-        reward = 0
-        curr_arrival = self.arrivals.iloc[self.curr_idx]
-        curr_time = curr_arrival['time']
-        self.curr_idx += 1
-        next_time = self.arrivals.time.iloc[self.curr_idx]
-
-        # Assign the resources based on the action
-        asked_cpu = curr_arrival['cpu']
-        asked_memory = curr_arrival['memory']
-        asked_disk = curr_arrival['disk']
-        print(f'asked resources = (CPU={asked_cpu}, mem={asked_memory},disk={asked_disk})')
-        if action == A_LOCAL:
-            if self.cpu < asked_cpu or self.memory < asked_memory or\
-                    self.disk < asked_disk:
-                print('local action but NO RESSSSSSSSS')
-                reward -= curr_arrival['reward']
-            else:
-                self.cpu -= asked_cpu
-                self.memory -= asked_memory
-                self.disk -= asked_disk
-                self.in_local.append(self.curr_idx)
-        elif action == A_FEDERATE:
-            if self.f_cpu < asked_cpu or self.f_memory < asked_memory or\
-                    self.f_disk < asked_disk:
-                print('federate action but NO RESSSSSSSSS')
-                reward -= curr_arrival['reward']
-            else:
-                self.f_cpu -= asked_cpu
-                self.f_memory -= asked_memory
-                self.f_disk -= asked_disk
-                self.in_fed.append(self.curr_idx)
-        elif action == A_REJECT:
-            pass
-
-        # calculate the reward from [t, t+1]
-        reward += self.__calc_reward(curr_time, next_time)
-        # services leave
-        self.__free_resources(next_time)
-
-
-        return reward, self.get_state() # it'll handle the episode END
-
-
-    def __get_pricing_intervals(self, prev_time, until, arrival_idx):
-        # creates a list of spot pricing betweeen [prev, until]
-        # the returned is a list of lists like
-        #   [ [time0, spot_price0], ... [timeN, spot_priceN] ]
-        # with time0>=prev_time and timeN<=until
-        #
-        #
-        # NOTE: this functions handles with the below corner cases in
-        #       which the prev and until values do not lie within the
-        #       SpotPrices history
-        #
-        #
-        #                 --- prev     
-        #                  |          --- prev
-        #                 --- until    | 
-        #    t1,$1 ---                 |
-        #           |                  |
-        #           |                 --- until
-        #           |     --- prev                  
-        #           |      |               
-        #           |      |                       
-        #    t2,$2 ---    ---
-        #           |      |           --- prev
-        #           |     --- until     |
-        #           |                   |
-        #           |                  --- until
-        #    t3,$3 ---
-        #     
-        #       SpotPrices
-        #
-        #def __get_pricing_intervals(self, prev_time, until, arrival_idx):
-
-        # Create timestamp versions for prev and until
-        prev_ts = pd.Timestamp(prev_time, unit='s', tz='UTC')
-        until_ts = pd.Timestamp(until, unit='s', tz='UTC')
-
-        # Get the spot prices history of the arrival instance
-        arrival = self.arrivals.iloc[arrival_idx]
-        spot_history = self.spot_prices[(self.spot_prices['InstanceType'] ==\
-                                        arrival['instance']) &\
-                                    (self.spot_prices['ProductDescription'] ==\
-                                    arrival['os'])]
-        spot_history = spot_history[['Timestamp', 'SpotPrice']]
-        spot_history.sort_values(by='Timestamp', ascending=True, inplace=True)
-        spot_historyFst = spot_history.iloc[0]
-
-        # DEBUG the head
-        # print('HHEAD')
-        # print(spot_history.head())
-        # print(f'PREV={pd.Timestamp(prev_time, unit="s")}, UNTIL={pd.Timestamp(until, unit="s")}')
-
-        # until < t1
-        if until < spot_historyFst['Timestamp'].timestamp():
-            return [[prev_ts, spot_historyFst['SpotPrice']],
-                    [until_ts,     spot_history.iloc[1]['SpotPrice']]]
-
-        spot_history_until = spot_history[
-                                spot_history['Timestamp'] <= until_ts]
-
-        # prev_time < t1  &  until >= t1
-        if prev_ts < spot_historyFst['Timestamp']:
-            ret_prices = spot_history_until.values
-            ret_prices = [list(sp_t) for sp_t in ret_prices]
-            print('prev_time < t1  &  until >= t1')
-            print(f'type(ret_prices[0][0])={type(ret_prices[0][0])}')
-            print('ret_prices')
-            print(ret_prices)
-            if len(spot_history_until) == 1:
-                ret_prices = [[prev_ts, spot_historyFst['SpotPrice']]] +\
-                             ret_prices
-            ret_prices[-1][0] = until_ts
-            return ret_prices
-
-        # prev_time >= t1  &  until >= t1
-        spot_history_between = spot_history_until[
-                spot_history_until['Timestamp'] >= prev_ts]
-
-        # no tn:  prev_time <= tn <= until
-        if len(spot_history_between) == 0:
-            spot_history_untilLst = spot_history_until.iloc[-1]
-            return [
-                [prev_ts, spot_history_untilLst['SpotPrice']],
-                [until_ts, spot_history_untilLst['SpotPrice']]
-            ]
-
-        # !E tn:  prev_time <= tn <= until
-        if len(spot_history_between) == 1:
-            spot_history_betweenLst = spot_history_between.iloc[-1]
-            return [
-                [prev_ts, spot_history_betweenLst['SpotPrice']],
-                [until_ts, spot_history_betweenLst['SpotPrice']]
-            ]
-
-        # E {tn, tn+1, ...}: prev_time <= tn <= until
-        ret_prices = spot_history_between.values
-        #print(f'ret_prices={ret_prices}')
-        ret_prices[0][0] = prev_ts
-        ret_prices[-1][0] = until_ts
-        return ret_prices
-
-
-
-    def __calc_arrival_reward(self, prev_time, curr_time, arrival_idx,
-                              federated):
-        # Compute the reward
-        arrival = self.arrivals.iloc[arrival_idx]
-        t_end = arrival['lifetime']*24*60*60 + arrival['time']
-        until = min(t_end, curr_time)
-        reward = self.arrivals.iloc[arrival_idx]['reward'] *\
-                 (until - prev_time) / (60*60) 
-
-        if not federated:
-            return reward
-
-        #######################################################################
-        # From here down we substract the spot price for the federated arrival
-        #######################################################################
-
-
-        pricing = self.__get_pricing_intervals(prev_time, until, arrival_idx)
-        print(f'FEDERATED arrival={arrival_idx}')
-        print(f'reward={reward}')
-        print(f'pricing={pricing}')
-        penalty = 0
-        for end, begin in zip(pricing[:-1], pricing[1:]):
-            delta = (end[0].timestamp() - begin[0].timestamp()) / (60*60) # h
-            print('\t\tpenaly iter')
-            penalty += delta * begin[1]
-            reward -= delta * begin[1]
-        print(f'\tpenalty={penalty}')
-
-
-        return reward
-
-
-    def __calc_reward(self, prev_time, curr_time):
-        # compute the reward in between [prev_time,curr_time]
-        # for local and federated instances
-        reward = 0
-
-        for local_idx in self.in_local:
-            reward += self.__calc_arrival_reward(prev_time, curr_time,
-                    local_idx, federated=False)
-        for fed_idx in self.in_fed:
-            reward += self.__calc_arrival_reward(prev_time, curr_time,
-                    fed_idx, federated=True)
-
-        return reward
-
-
-    def __free_resources(self, curr_time):
-        remove_local, remove_fed  = [], []
-
-        print(f' ENV:: local deployed={self.in_local}')
-        print(f' ENV:: fed deployed={self.in_fed}')
-
-        # Check local arrivals that have expired
-        for local_idx in self.in_local:
-            expires = self.arrivals.iloc[local_idx]['time'] +\
-                self.arrivals.iloc[local_idx]['lifetime']*24*60*60
-            if expires <= curr_time:
-                remove_local.append(local_idx)
-
-        # Check federated arrivals that have expired
-        for fed_idx in self.in_fed:
-            expires = self.arrivals.iloc[fed_idx]['time'] +\
-                self.arrivals.iloc[fed_idx]['lifetime']*24*60*60
-            if expires <= curr_time:
-                remove_fed.append(fed_idx)
-
-        # Remove the arrivals from the lists, and free resources
-        for rem_loc_idx in remove_local:
-            print(f'remove local idx = {rem_loc_idx}')
-            print(f'loc list= {self.in_local}')
-            del self.in_local[self.in_local.index(rem_loc_idx)]
-            self.cpu    += self.arrivals.iloc[rem_loc_idx]['cpu']
-            self.disk   += self.arrivals.iloc[rem_loc_idx]['disk']
-            self.memory += self.arrivals.iloc[rem_loc_idx]['memory']
-        for rem_fed_idx in remove_fed:
-            print(f'remove federeated idx = {rem_fed_idx}')
-            print(f'fed list= {self.in_fed}')
-            del self.in_fed[self.in_fed.index(rem_fed_idx)]
-            self.f_cpu    += self.arrivals.iloc[rem_fed_idx]['cpu']
-            self.f_disk   += self.arrivals.iloc[rem_fed_idx]['disk']
-            self.f_memory += self.arrivals.iloc[rem_fed_idx]['memory']
-
-
-    def reset(self):
-        self.__free_resources(curr_time=FUTURE.timestamp())
-        self.curr_idx = 0
-
-
-
 
 
 
@@ -405,10 +58,10 @@ def create_q_network(k):
         keras.layers.Dense(k*11, activation='tanh'),
         #keras.layers.Dense(k*11, activation='sigmoid'),
         #keras.layers.Dense(k*11),
-        #keras.layers.Dense(len(ACTIONS), activation='sigmoid')
-        #keras.layers.Dense(len(ACTIONS), activation='tanh')
-        #keras.layers.Dense(len(ACTIONS), activation='relu')
-        keras.layers.Dense(len(ACTIONS))
+        #keras.layers.Dense(len(AWS_env.ACTIONS), activation='sigmoid')
+        #keras.layers.Dense(len(AWS_env.ACTIONS), activation='tanh')
+        #keras.layers.Dense(len(AWS_env.ACTIONS), activation='relu')
+        keras.layers.Dense(len(AWS_env.ACTIONS))
     ])
 
     print('Just have created the NN, check below the TF summary')
@@ -454,7 +107,7 @@ def atari_loss(model, transitions, training, k, gamma):
         pred = model(phi)
 
         # Set y=pred for a!=action
-        for a in ACTIONS:
+        for a in AWS_env.ACTIONS:
             if a != action:
                 y[0][a] = pred.numpy()[0][a]
         # y = tf.constant(y) # TODO: check if changing this to constant
@@ -540,11 +193,12 @@ def train_q_network(model, k, epsilon_start, epsilon_end, gamma, alpha, M,
             # epsilon-greedy action selection
             action = 0
             if random.random() < epsilon:
-                action = ACTIONS[random.randint(0, len(ACTIONS) - 1)]
+                action = AWS_env.ACTIONS[random.randint(0,
+                                         len(AWS_env.ACTIONS) - 1)]
                 print(f'ϵ-greedy: random action={action}')
             else:
                 Q = model(now_phi)
-                action = ACTIONS[Q[0].numpy().argmax()]
+                action = AWS_env.ACTIONS[Q[0].numpy().argmax()]
                 print(f'ϵ-greedy: max action={action}')
                 #print(f'\tmax all action values={Q}')
 
